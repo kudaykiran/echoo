67ea80fd3078be75a093bae94abbd25657718c63
AKSAHA <sheol.aks@gmail.com>
2020-05-18 23:18:54 +0100

    updated unit test

--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
 var assert = require('assert');
+var utils = require('../server/utils/index');
+
 describe('Array', function () {
     describe('#indexOf()', function () {
         it('should return -1 when the value is not present', function () {
@@ -6,3 +8,13 @@ describe('Array', function () {
         });
     });
 });
+
+describe('index', function () {
+    it('should return \'\' when the page is not present', function () {
+        assert.equal(utils.pageQuery(), '');
+    });
+    it('should return value when the page is  present', function () {
+        assert.equal(utils.pageQuery('abc'), '&page=abc');
+    });
+});
+
